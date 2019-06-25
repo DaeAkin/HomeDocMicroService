33e279339389fd9432bed1bf0ed6cd8d7f9a26a2
민동현 <mindonghyeon890@gmail.com>
2019-06-25 13:16:23 +0900

    initial commit

---
+++ HomeDoc-MicroService-Database/src/main/java/www/com/homedoc/HomeDocMicroServiceDatabaseApplication.java
@@ -0,0 +1,13 @@
+package www.com.homedoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDocMicroServiceDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeDocMicroServiceDatabaseApplication.class, args);
+	}
+
+}
